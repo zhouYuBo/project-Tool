fcd8e8c934cbc6c9e723776e8d7b7aaafc2e3364
zhouyubo03 <zhouyubo03@baidu.com>
2018-11-04 01:02:47 +0800

    project-create
    project-create

---
+++ Progect/Progect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Progect
+//
+//  Created by Zhou,Yubo on 2018/11/4.
+//  Copyright © 2018 Zhou,Yubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
